fc3c3acb00334d86427e8803dacfd7c02036eab1
Corbfon <corbin.fonville@gmail.com>
2018-05-03 13:31:37 -0500

    configure prerender

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const prerender = require('prerender')
 
-const app = express()
-app.get('/', (req, res) => {
-  res.status(200).send({ message: 'hello there' })
+const app = prerender({
+  logRequests: true
 })
+app.start()
 
-const PORT = process.env.PORT || '5555'
+// const app = express()
+// app.get('/', (req, res) => {
+//   res.status(200).send({ message: 'hello there' })
+// })
 
-app.listen(PORT, () => {
-  console.log('prerender server listening on ' + PORT)
-})
+// const PORT = process.env.PORT || '5555'
+
+// app.listen(PORT, () => {
+//   console.log('prerender server listening on ' + PORT)
+// })
