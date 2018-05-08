f97104689458e88d4d15850998c08400298730bb
Corbfon <corbin.fonville@gmail.com>
2018-05-08 14:59:14 -0500

    try installing chrome

--- simple-express-app/build/package.sh
+++ simple-express-app/build/package.sh
@@ -62,6 +62,12 @@ n use ${NODE_VER}
 npm install
 npm install forever -g
 
+# install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
 # configure the AMI built-in apache2
 a2ensite node-express.conf
 a2dissite 000-default.conf
