72fab3c18e750c8717596f9e8e29dd68ce3c740f
Corbfon <corbin.fonville@gmail.com>
2018-05-10 12:12:28 -0500

    run chrome with new tag list

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const prerender = require('prerender')
 
 const app = prerender({
-  logRequests: true
+  logRequests: true,
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
 })
 app.start()
 
