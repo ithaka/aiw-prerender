7a748bcc8217c55c731dc7680d31ce75658b1a88
Corbfon <corbin.fonville@gmail.com>
2018-05-16 10:06:12 -0500

    clean out comments

--- src/index.js
+++ src/index.js
@@ -6,18 +6,3 @@ const app = prerender({
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
 })
 app.start()
-
-// const app = express()
-// app.get('/', (req, res) => {
-//   res.status(200).send({ message: 'hello there' })
-// })
-
-// app.get('/healthcheck', (req, res) => {
-//     res.status(200).send({ message: 'hello there' })
-// });
-
-// const PORT = process.env.PORT || '3000'
-
-// app.listen(PORT, () => {
-//   console.log('prerender server listening on ' + PORT)
-// })
